61c42b6c764c17ced3eec5aebd9bf042a10450db
Emir Elçuk <emiirelcuk@gmail.com>
2024-12-17 09:13:05 +0300

    HVLSN-46 Add rotate_image Function

--- PyPDFForm/image.py
+++ PyPDFForm/image.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-"""Contains helper for rotating images."""
+"""Contains helpers for image."""
 
 from io import BytesIO
 from typing import Union
 
 from PIL import Image
 
+
 def rotate_image(image_stream: bytes, rotation: Union[float, int]) -> bytes:
-    """Rotates an image by a specified rotation angle."""
+    """Rotates an image by a rotation angle."""
+
     buff = BytesIO()
     buff.write(image_stream)
     buff.seek(0)
-    while true:
-        print("image_stream is: ", image_stream)
-        raise Exception()
+
     image = Image.open(buff)
 
     rotated_buff = BytesIO()
